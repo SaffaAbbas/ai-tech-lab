27e4318c82840ad57c000460063cbe2e6d41c285
SaffaAbbas <safaabbas167@gmail.com>
2026-03-01 19:50:43 +0500

    phone number added

--- src/app/components/CalendarSection.jsx
+++ src/app/components/CalendarSection.jsx
@@ -372,7 +372,7 @@ export default function CalendarSection() {
                       name="phone"
                       value={formData.phone}
                       onChange={handleChange}
-                      placeholder="+92"
+                      placeholder="Enter Phone Number"
                       className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     />
                   </div>
